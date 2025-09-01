c1d70d2980f11a09c998c0d97bd914899662087f
Glen Hunter <29704413+ghhunter@users.noreply.github.com>
2025-09-01 10:33:54 +0200

    test: restart integration test (#17)
    * Initial test commit
    
    * Removed unneeded line
    
    * Reformatted location of .ff directory, removed unneeded comments and style: black
    
    * removed slow mark from integration test
    
    * added pytest to CI pipeline
    
    * fix: typo
    
    * fix: typo
    
    * fix: get kimdy-grappa
    
    * fix: installation of kimmdy-grappa in tests for CI
    
    * readded slow mark as integration test takes too long
    
    * Implimeneted requested changes
    
    * feat: unit tests for rate calculations
    
    * style: black
    
    * test: restart integration tests
    
    * style: black

--- tests/test_hydrolysis_integration.py
+++ tests/test_hydrolysis_integration.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import pytest
 
 from kimmdy.cmd import kimmdy_run
+from kimmdy.utils import get_task_directories
+from kimmdy.constants import MARK_DONE, MARK_FINISHED
 
 
 def read_last_line(file):
@@ -27,3 +29,37 @@ def test_integration_hydrolysis_reaction(arranged_tmp_path):
 
     assert "Finished running last task" in read_last_line(Path("run_prod.kimmdy.log"))
     assert len(list(Path.cwd().glob("run_prod/*"))) == 11
+
+
+@pytest.mark.parametrize(
+    "arranged_tmp_path", (["test_hydrolysis_integration"]), indirect=True
+)
+@pytest.mark.slow
+def test_integration_hydrolysis_restart(arranged_tmp_path):
+    run_dir = Path("run_prod")
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    n_files_original = len(list(run_dir.glob("*")))
+
+    # restart already finished run
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    assert "already finished" in read_last_line(Path("run_prod.kimmdy.log"))
+
+    # try restart from stopped md
+    task_dirs = get_task_directories(run_dir)
+    (task_dirs[-1] / MARK_DONE).unlink()
+    (arranged_tmp_path / run_dir / MARK_FINISHED).unlink()
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    n_files_continue_md = len(list(run_dir.glob("*")))
+
+    assert "Finished running last task" in read_last_line(Path("run_prod.kimmdy.log"))
+    assert n_files_original == n_files_continue_md == 11
+
+    # try restart from finished md
+    task_dirs = get_task_directories(run_dir)
+    (task_dirs[-4] / MARK_DONE).unlink()
+    (arranged_tmp_path / run_dir / MARK_FINISHED).unlink()
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    n_files_restart = len(list(run_dir.glob("*")))
+
+    assert "Finished running last task" in read_last_line(Path("run_prod.kimmdy.log"))
+    assert n_files_original == n_files_restart == 11
